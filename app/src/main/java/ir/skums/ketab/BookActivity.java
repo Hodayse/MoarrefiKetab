2d5f984e6fdaa62ed237c66a7caa03a9a66a5da4
TSS <tss@TSSs-MacBook.local>
2021-12-18 14:37:16 +0330

    first commit

---
+++ app/src/main/java/ir/skums/ketab/BookActivity.java
@@ -0,0 +1,14 @@
+package ir.skums.ketab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book);
+    }
+}
